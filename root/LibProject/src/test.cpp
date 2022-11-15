777081540c75745ce83b3e36b8cb4f73e561fa72
muralireddy34 <100593612+muralireddy34@users.noreply.github.com>
2022-11-15 12:09:09 +0530

    Update test.cpp

--- root/LibProject/src/test.cpp
+++ root/LibProject/src/test.cpp
@@ -1,8 +1,27 @@
-#include<iostream>    
-using namespace std;    
+#include <stdio.h>
+#include <math.h>
 
-int main()    
-{    
-    cout<<"Hello World"<<endl;    
-    return 0;    
+const double a;
+double b;
+
+
+
+double squareRoot(const double a) {
+    double b = sqrt(a);
+    if(b != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
+
+
+
+
+int main()
+{
+    scanf("%f", &a);
+    b = squareRoot(a);
+    printf("%f", b);
+    return 0;
 }
